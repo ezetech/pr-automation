e10498bb5da046cdec9f55ac476b3a93b9443490
lasha petriashvili <lasha.petro1@gmail.com>
2023-03-01 16:42:18 +0400

    Add jira logic in auto-merge.ts

--- src/actions/auto-merge.ts
+++ src/actions/auto-merge.ts
@@ -4,6 +4,7 @@ import * as github from '@actions/github';
 import { Inputs, Strategy } from '../config/typings';
 import { info, error, warning } from '../logger';
 import { isPrFullyApproved } from '../approves/is-pr-fully-approved';
+import changeJiraIssueStatus from '../jira/change-jira-issue-status';
 
 export async function run(): Promise<void> {
   try {
@@ -20,6 +21,11 @@ export async function run(): Promise<void> {
       strategy: core.getInput('strategy', { required: true }) as Strategy,
       doNotMergeLabels: core.getInput('do-not-merge-labels'),
       token: core.getInput('token', { required: true }),
+      jiraToken: core.getInput('jira-token', { required: true }),
+      jiraAccount: core.getInput('jira-account', { required: true }),
+      jiraEndpoint: core.getInput('jira-endpoint', { required: true }),
+      jiraMoveIssueFrom: core.getInput('jira-move-issue-from', { required: true }),
+      jiraMoveIssueTo: core.getInput('jira-move-issue-to', { required: true }),
     };
 
     const client = github.getOctokit(configInput.token);
@@ -64,14 +70,27 @@ export async function run(): Promise<void> {
       core.setOutput('commentID', resp.id);
     }
 
-    await client.pulls.merge({
-      owner,
-      repo,
-      pull_number: configInput.pullRequestNumber,
-      merge_method: configInput.strategy,
-    });
+    const branchName = pullRequest.head.ref;
+    const baseBranchName = pullRequest.base.ref;
 
-    info(`Merged pull request #${configInput.pullRequestNumber}`);
+    if (baseBranchName !== 'master' && baseBranchName !== 'main') {
+      await client.pulls.merge({
+        owner,
+        repo,
+        pull_number: configInput.pullRequestNumber,
+        merge_method: configInput.strategy,
+      });
+
+      info(`Merged pull request #${configInput.pullRequestNumber}`);
+    }
+
+    const jiraResponse = await changeJiraIssueStatus(branchName, configInput);
+
+    if (jiraResponse.status) {
+      info(jiraResponse.message);
+    } else {
+      warning(jiraResponse.message);
+    }
 
     core.setOutput('merged', true);
   } catch (err) {
